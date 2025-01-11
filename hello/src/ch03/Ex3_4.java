714274905251aa943aca4fea617c59dfe7952a9f
sy-0922 <soychoi939@gmail.com>
2025-01-12 00:30:47 +0900

    ch03 commit

---
+++ hello/src/ch03/Ex3_4.java
@@ -0,0 +1,15 @@
+package ch03;
+
+public class Ex3_4 {
+
+	public static void main(String[] args) {
+		int i = -10;
+		i = +i;
+		System.out.println(i);
+
+		i = -10;
+		i = -i;
+		System.out.println(i);
+	}
+
+}
