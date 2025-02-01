f23a10cde8bfcbcb7af921a28b27fe0946c19409
sy-0922 <soychoi939@gmail.com>
2025-02-02 00:05:17 +0900

    ch04 commit

---
+++ hello/src/ch04/Ex4_8.java
@@ -0,0 +1,10 @@
+package ch04;
+
+public class Ex4_8 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 3; i++) { // °ýÈ£{}¾ÈÀÇ ¹®ÀåÀ» 3¹ø ¹Ýº¹
+			System.out.println("Hello");
+		}
+	}
+}
