a9c40897f83718f1f4980a5116a97c08c1d6d381
sy-0922 <soychoi939@gmail.com>
2025-02-27 23:00:47 +0900

    study

---
+++ hello/src/ch06/Ex6_14.java
@@ -0,0 +1,23 @@
+package ch06;
+
+class Ex6_14 {
+	static {
+		System.out.println("static { }");
+	}
+
+	{
+		System.out.println("{ }");
+	}
+
+	public Ex6_14() {
+		System.out.println("constructor");
+	}
+
+	public static void main(String args[]) {
+		System.out.println("Ex6_14 bt = new Ex6_14(); ");
+		Ex6_14 bt = new Ex6_14();
+
+		System.out.println("Ex6_14 bt2 = new Ex6_14(); ");
+		Ex6_14 bt2 = new Ex6_14();
+	}
+}
