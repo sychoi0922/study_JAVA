ef991dc8cdfc48cfd75bc5454952b5b5bca65b8e
sy-0922 <soychoi939@gmail.com>
2025-02-28 01:13:08 +0900

    commit

--- hello/src/ch06/Ex6_14.java
+++ hello/src/ch06/Ex6_14.java
@@ -15,7 +15,7 @@ class Ex6_14 {
 
 	public static void main(String args[]) {
 		System.out.println("Ex6_14 bt = new Ex6_14(); ");
-		Ex6_14 bt = new Ex6_14();
+		Ex6_14 bt = new Ex6_14(); 
 
 		System.out.println("Ex6_14 bt2 = new Ex6_14(); ");
 		Ex6_14 bt2 = new Ex6_14();
