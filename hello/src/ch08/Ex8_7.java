086f8d00057017ccc9f40b16196feaa3734c480d
sy-0922 <soychoi939@gmail.com>
2025-03-13 15:15:34 +0900

    java study

---
+++ hello/src/ch08/Ex8_7.java
@@ -0,0 +1,7 @@
+package ch08;
+
+class Ex8_7 {
+	public static void main(String[] args) {
+//		throw new Exception();		// Exception을 고의로 발생시킨다.
+	}
+}
---
+++ hello/src/ch08/Ex8_8.java
@@ -0,0 +1,7 @@
+package ch08;
+
+class Ex8_8 {
+	public static void main(String[] args) {
+		throw new RuntimeException();	// RuntimeException을 고의로 발생시킨다.
+	}
+}
