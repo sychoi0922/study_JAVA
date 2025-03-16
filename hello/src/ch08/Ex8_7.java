53c211cd1a7a09bd85009b1fde746a7cae22c351
sy-0922 <soychoi939@gmail.com>
2025-03-17 00:03:19 +0900

    study

--- hello/src/ch08/Ex8_7.java
+++ hello/src/ch08/Ex8_7.java
@@ -2,6 +2,6 @@ package ch08;
 
 class Ex8_7 {
 	public static void main(String[] args) {
-//		throw new Exception();		// Exception을 고의로 발생시킨다.
+//		throw new Exception();		// Exception을 고의로 발생시킨다. -> checked 예외인데 try catch 예외처리를 안해줘서 빨간 줄 뜨는 것
 	}
 }
